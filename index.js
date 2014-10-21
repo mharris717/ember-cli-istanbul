fc2f72c6f7bd20245a23767379927f03c12dffde
Mike Harris <mharris717@gmail.com>
2014-10-21 16:25:50 -0400

    CS

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 'use strict';
 
+var Filter = require('broccoli-filter')
+
+function IstanbulFilter(inputTree,options) {
+  if (!(this instanceof IstanbulFilter)) return new IstanbulFilter(inputTree,options);
+  Filter.call(this,inputTree,options)
+  options = options || {}
+
+}
+
+IstanbulFilter.prototype = Object.create(Filter.prototype);
+IstanbulFilter.prototype.constructor = IstanbulFilter
+
+IstanbulFilter.prototype.extensions = ['js']
+IstanbulFilter.prototype.targetExtensions = 'js'
+
+IstanbulFilter.prototype.processString = function(string) {
+  console.log("processString")
+  return string
+}
+
+
+function IstanbulPreprocessor(options) {
+  this.name = 'ember-cli-istanbul';
+  this.ext = 'js';
+  this.options = options || {};
+}
+
+IstanbulPreprocessor.prototype.toTree = function(tree, inputPath, outputPath) {
+  console.log("toTree");
+  var options = {
+    bare: true,
+    srcDir: inputPath,
+    destDir: outputPath
+  };
+
+  return IstanbulFilter(tree,options);
+};
+
 module.exports = {
-  name: 'ember-cli-istanbul'
+  name: 'ember-cli-istanbul',
+
+  included: function(app) {
+    console.log("included");
+
+    this.app = app;
+
+    var plugin = new IstanbulPreprocessor({});
+
+    this.app.registry.add('js', plugin);
+  }
 };
