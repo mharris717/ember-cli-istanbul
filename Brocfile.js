19479231e796de25d410096d0b9a8b06bb2d913a
Mike Harris <mharris717@gmail.com>
2014-10-22 16:10:27 -0400

    CS

--- Brocfile.js
+++ Brocfile.js
@@ -1,4 +1,15 @@
 /* global require, module */
+//var ES6Transpiler = require('es6-module-transpiler').Compiler
+
+
+// function testStuff() {
+  
+//   var compiler = new ES6Transpiler("export default 14;", "dummy/fun")
+//   var res = compiler.toAMD()
+//   console.log(res)
+// }
+
+//testStuff()
 
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
@@ -14,12 +25,16 @@ var beforeFunc = function(string,ops) {
 
   var res = string;
 
-  if (ops.moduleName == "dummy/models/doubler") {
-    res = instrumenter.instrumentSync(string, "/code/orig/ember-cli-istanbul/transpiled_js/dummy/models/doubler.js");
-    // res = instrumenter.instrumentSync(string, "/code/orig/ember-cli-istanbul/big.js");
-    //res = instrumenter.instrumentSync(string, "/code/orig/ember-cli-istanbul/tests/dummy/app/models/doubler.js");
-    res = res.replace("Function(\'return this\')","Function(\'return thisBeforeEval\')");
-  }
+  // if (ops.moduleName == "dummy/models/doubler") {
+  //   res = instrumenter.instrumentSync(string, "/code/orig/ember-cli-istanbul/transpiled_js/dummy/models/doubler.js");
+  //   // res = instrumenter.instrumentSync(string, "/code/orig/ember-cli-istanbul/big.js");
+  //   //res = instrumenter.instrumentSync(string, "/code/orig/ember-cli-istanbul/tests/dummy/app/models/doubler.js");
+  //   res = res.replace("Function(\'return this\')","Function(\'return thisBeforeEval\')");
+  // }
+
+  var transpiledPath = "/code/orig/ember-cli-istanbul/transpiled_js/" + ops.moduleName + ".js"
+  res = instrumenter.instrumentSync(string, transpiledPath);
+  res = res.replace("Function(\'return this\')","Function(\'return thisBeforeEval\')");
   
 
   
